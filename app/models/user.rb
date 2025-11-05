87971555a0416900c93271b54e4f7f50f030c4a3
abruskin <90646708+abruskin@users.noreply.github.com>
2025-11-05 15:05:00 -0600

    added message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-     validates :username, presence: true, 
+    validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+    has_many :messages, dependent: :destroy
     has_secure_password
 end
