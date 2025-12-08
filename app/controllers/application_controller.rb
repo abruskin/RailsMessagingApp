4f75d24ae14dd5340917e12661a300a084ff041b
abruskin <90646708+abruskin@users.noreply.github.com>
2025-12-08 12:21:05 -0600

    flash messages now work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  add_flash_types :info, :error, :success
     helper_method :current_user, :logged_in?
   
   def current_user
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:alert] = "You must be logged in to perform that action"
+      flash[:error] = "You must be logged in to perform that action"
       redirect_to login_path
     end 
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,17 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:session][:username])
+        puts user.inspect
         if user && user.authenticate(params[:session][:password])
+            puts "Authentication successful"
             session[:user_id] = user.id
-            flash[:success] = "Login successful"
+            flash[:notice] = "Login successful"
 
             redirect_to root_path
         else
+            puts "Authentication failed"
             flash.now[:error] = "Username and/or password not recognized"
-            render 'new'
+            render 'new', status: :unprocessable_entity
         end
     end
 
@@ -19,7 +22,7 @@ class SessionsController < ApplicationController
      
         session[:user_id] = nil
         session[:params]=nil
-        flash[:success]="Logged out"
+        flash[:notice]="Logged out"
         
         redirect_to login_path
     end
